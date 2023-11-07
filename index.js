089b7d727190ad0a857571c509a7bf934ab2ac34
Ipan Nugraha <nugrahaipan55@gmail.com>
2023-11-07 10:22:34 +0700

    menambahkan fitur filter dan input gambar dengan file

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser'); // Pastikan Anda telah menginstal middleware body-parser
 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads'); // Ganti dengan direktori yang sesuai dengan proyek Anda
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 // Konfigurasi view engine untuk menggunakan EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "/public")))
+
 
 // Middleware untuk mengurai body permintaan JSON
 app.use(bodyParser.json());
@@ -16,15 +31,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const { Mobil } = require('./models');
 
 // Rute untuk menampilkan daftar mobil
+
 app.get('/mobil', async (req, res) => {
   try {
     const mobilList = await Mobil.findAll();
-    res.render('list', { mobilList });
+    const notificationMessage = req.query.notificationMessage; // Ambil pesan notifikasi dari query string jika ada
+    res.render('list', { mobilList, notificationMessage });
   } catch (error) {
     console.error(error);
     res.status(500).send('Terjadi kesalahan dalam mengambil data mobil.');
   }
 });
+
+
 
 // Rute untuk menampilkan halaman tambah mobil
 app.get('/mobil/tambah', (req, res) => {
@@ -32,11 +51,20 @@ app.get('/mobil/tambah', (req, res) => {
 });
 
 // Rute untuk menangani penambahan data mobil
-app.post('/mobil/tambah', async (req, res) => {
+app.post('/mobil/tambah', upload.single('images'), async (req, res) => {
   try {
-    const { name, price, category, images } = req.body;
-    await Mobil.create({ name, price, category, images });
-    res.redirect('/mobil');
+    const { name, price, category } = req.body;
+    const imagePath = req.file.path; // Lokasi gambar yang diunggah
+
+    // Simpan data mobil dan lokasi gambar ke dalam database
+    await Mobil.create({ name, price, category, images: imagePath });
+
+    // Tambahkan notifikasi di sini
+    const notificationMessage = 'Data mobil berhasil ditambahkan!';
+    
+    // Setelah menambahkan data, arahkan ke halaman daftar mobil
+    const mobilList = await Mobil.findAll();
+    res.render('list', { mobilList, notificationMessage });
   } catch (error) {
     console.error(error);
     res.status(500).send('Terjadi kesalahan dalam menambahkan data mobil.');
@@ -88,7 +116,8 @@ app.get('/mobil/hapus/:id', async (req, res) => {
     const mobil = await Mobil.findByPk(id);
     if (mobil) {
       await mobil.destroy();
-      res.redirect('/mobil');
+      const notificationMessage = 'Data mobil berhasil dihapus!';
+      res.redirect('/mobil?notificationMessage=' + encodeURIComponent(notificationMessage)); // Redirect ke /mobil dengan pesan notifikasi
     } else {
       res.status(404).send('Data mobil tidak ditemukan.');
     }
